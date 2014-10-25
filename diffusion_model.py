a37d5ce17dcf4177d987c94d7728acd1d050d1e9
TV <mye.mail@gmx.net>
2014-10-25 21:05:03 +0100

    First commit.

---
+++ diffusion_model.py
@@ -0,0 +1,12 @@
+def energy(density, coeff=1.0):
+  """ Energy associated with the diffusion model
+
+      Parameters
+      ----------
+
+      density: array of positive integers
+          Number of particles at each position i in the array
+      coeff: float
+          Diffusion coefficient.
+  """
+  # implementation goes here
---
+++ test_diffusion_model.py
@@ -0,0 +1,4 @@
+from diffusion_model import energy
+def test_energy():
+  """ Optional description for nose reporting """
+  # Test something
