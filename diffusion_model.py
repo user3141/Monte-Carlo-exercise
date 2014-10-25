422f36f887d61b27ffac4ffd05d6a422f219cb22
TV <mye.mail@gmx.net>
2014-10-25 21:22:54 +0100

    Energy function + unit tests

--- diffusion_model.py
+++ diffusion_model.py
@@ -9,4 +9,21 @@ def energy(density, coeff=1.0):
       coeff: float
           Diffusion coefficient.
   """
-  # implementation goes here
+  from numpy import array, any, sum
+  
+  # Make sure input is an array
+  density = array(density)
+  # of the right kind (integer). Unless it is zero length, in which case type does not matter.
+  if density.dtype.kind != 'i' and len(density) > 0:
+    raise TypeError("Density should be an array of *integers*.")
+  # and the right values (positive or null)
+  if any(density < 0):
+    raise ValueError("Density should be an array of *positive* integers.")
+  if density.ndim != 1:
+    raise ValueError("Density should be an a *1-dimensional* array of positive integers.")
+    
+  return coeff * 0.5 * sum(density * (density - 1))
+  
+
+if __name__ == "__main__":
+  energy([1,2,3,4,5])
--- test_diffusion_model.py
+++ test_diffusion_model.py
@@ -1,4 +1,12 @@
 from diffusion_model import energy
+from nose.tools import assert_equal, assert_raises, assert_true
+
 def test_energy():
   """ Optional description for nose reporting """
-  # Test something
+  density = [2,1]
+  assert_equal(energy(density), 1.)
+  assert_equal(energy(density, coeff=2), 2.)
+  assert_raises(energy([]))
+  assert_equal(energy([3,4,5]), 19.)
+  assert_equal(type(energy([1])), type(1.))
+  
