7c478d0505ac2764488bd99cd0901924d978a5d0
TV <mye.mail@gmx.net>
2014-10-26 18:29:16 +0000

    Always use numpy arrays, avoid conversion.

--- monte_carlo.py
+++ monte_carlo.py
@@ -11,29 +11,29 @@ def accumulate(lis):
 
 def move_particle(density):
   """Moves random particle randomly to the left or right."""
-  
+
   while True:
     # choose random particle
     particle = random.randint(1, np.sum(density))
     # find position of particle
     cum_particles = list(accumulate(density))
     pos = [i for i in xrange(len(cum_particles)) if cum_particles[i] >= particle][0]
-     
+
     # random direction (-1 - left, 1 - right)
     direction = random.choice([-1,1])
-    
+
     # no periodic boundary conditions
     if pos == 0 and direction == -1:
       continue
     elif pos == len(density) - 1 and direction == 1:
       continue
-      
+
     # move particle
-    new_density = list(density)
+    new_density = np.array(density)
     new_density[pos] -= 1
     new_density[pos + direction] += 1
     break
-  
+
   return new_density
 
 
@@ -42,7 +42,7 @@ def accept_higher_energy(E_0, E_1, temp):
   boltzmann_factor = math.exp(- (E_1 - E_0)/temp)
   return random.random() < boltzmann_factor
 
-  
+
 def monte_carlo_sim(density, energy_fun, temp=300, steps=50000):
   """Monte Carlo (Metropolis-Hastings-algorithm) simulation of 1D diffusion.
      density - 1D particle density
@@ -52,7 +52,7 @@ def monte_carlo_sim(density, energy_fun, temp=300, steps=50000):
   """
   energies = [energy_fun(density)]
   densities = [density]
-  
+
   for i in xrange(steps):
     E_0 = energy_fun(density)
     new_density = move_particle(density)
@@ -62,25 +62,24 @@ def monte_carlo_sim(density, energy_fun, temp=300, steps=50000):
       energies.append(E_1)
       densities.append(density)
   return energies, densities
-      
-  
+
+
 if __name__ == "__main__"  :
   import matplotlib
   import matplotlib.pyplot as plt
   matplotlib.interactive(True)
-  
+
   density = [0] * 8 + [10] * 6 + [0] * 8
+  density = np.array(density)
   #random.shuffle(density)
   temp = 300
   energies, densities = monte_carlo_sim(density, dm.energy, temp, steps=5000)
   plt.plot(energies)
-  print densities[-10:-1]
   plt.savefig('monte_carlo.png')
-  
+
   densities = np.array(densities)
   plt.clf()
   heatmap = plt.pcolor(densities)
   plt.savefig('heatmap.png')
-  
-      
-  
+
+
